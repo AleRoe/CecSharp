5a99386d851446c73c58afcf628cbbc11e449e63
Alexander Röthinger <alexander1967muc@hotmail.com>
2021-04-04 00:04:53 +0200

    Renamed from MenuStatus

---
+++ AleRoe.CecSharp/Model/MenuState.cs
@@ -0,0 +1,11 @@
+﻿namespace AleRoe.CecSharp.Model
+{
+    /// <summary>
+    /// CEC menu status values.
+    /// </summary>
+    public enum MenuState : byte
+    {
+        Activated = 0x00,
+        Deactivated = 0x01
+    }
+}
