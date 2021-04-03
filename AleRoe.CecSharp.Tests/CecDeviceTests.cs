c2a5aa7a1d89f6b27dc91e95432933d3f20207ee
Alexander Röthinger <alexander1967muc@hotmail.com>
2021-04-04 00:11:11 +0200

    Added tests for all CecMessageBuilder methods. Added tests for all ProcessCecMessage cases.

--- AleRoe.CecSharp.Tests/CecDeviceTests.cs
+++ AleRoe.CecSharp.Tests/CecDeviceTests.cs
@@ -1,4 +1,6 @@
-﻿using AleRoe.CecSharp.Model;
+﻿using System;
+using System.Globalization;
+using AleRoe.CecSharp.Model;
 using NUnit.Framework;
 
 namespace AleRoe.CecSharp.Tests
@@ -7,7 +9,7 @@ namespace AleRoe.CecSharp.Tests
     public class CecDeviceTests
     {
         [Test()]
-        public void CtorTest()
+        public void CtorTest_0()
         {
             var device = new CecDevice(DeviceType.PlaybackDevice, "SomeName", 999);
             Assert.AreEqual(DeviceType.PlaybackDevice, device.DeviceType);
@@ -18,7 +20,7 @@ namespace AleRoe.CecSharp.Tests
         }
 
         [Test()]
-        public void CtorTest1()
+        public void CtorTest_1()
         {
             var device = new CecDevice(DeviceType.PlaybackDevice, "SomeName", 999,PhysicalAddress.Parse("2.1.0.0"));
             Assert.AreEqual(DeviceType.PlaybackDevice, device.DeviceType);
@@ -29,26 +31,320 @@ namespace AleRoe.CecSharp.Tests
         }
 
         [Test()]
-        public void CtorTest2()
+        public void CtorTest_2()
         {
-            var device = new CecDevice(DeviceType.PlaybackDevice, "SomeName", 999, PhysicalAddress.Parse("2.1.0.0"), LogicalAddress.Tuner3);
+            var device = new CecDevice(DeviceType.PlaybackDevice, "SomeName", 999, PhysicalAddress.Parse("2.1.0.0"), LogicalAddress.PlaybackDevice1);
             Assert.AreEqual(DeviceType.PlaybackDevice, device.DeviceType);
             Assert.AreEqual("SomeName", device.OsdName);
             Assert.AreEqual(999, device.VendorId);
             Assert.AreEqual(PhysicalAddress.Parse("2.1.0.0"), device.PhysicalAddress);
-            Assert.AreEqual(LogicalAddress.Tuner3, device.LogicalAddress);
+            Assert.AreEqual(LogicalAddress.PlaybackDevice1, device.LogicalAddress);
         }
 
         [Test()]
-        public void ProcessCecMessageTest()
+        public void CtorTest_DeviceTypeMismatchThrows()
         {
-            var device = new CecDevice(DeviceType.PlaybackDevice, "SomeName", 999, PhysicalAddress.Parse("2.1.0.0"), LogicalAddress.Tuner3);
-            var message = CecMessageBuilder.GivePhysicalAddress(LogicalAddress.TV, LogicalAddress.Tuner3);
-
-            var response = device.ProcessCecMessage(message);
-            Assert.AreEqual(CecMessageBuilder.ReportPhysicalAddress(LogicalAddress.Tuner3, DeviceType.PlaybackDevice,
-                PhysicalAddress.Parse("2.1.0.0")), response);
-
+            Assert.Throws<InvalidOperationException>(() => _ = new CecDevice(DeviceType.PlaybackDevice, "SomeName", 999,
+                PhysicalAddress.Parse("2.1.0.0"), LogicalAddress.Tuner3));
         }
+
+        [Test()]
+        public void CtorTest_OSDNameTooLongThrows()
+        {
+            Assert.Throws<ArgumentException>(() => _ = new CecDevice(DeviceType.PlaybackDevice, "SomeVeryLongName", 999,
+                PhysicalAddress.Parse("2.1.0.0"), LogicalAddress.Tuner3));
+        }
+
+        [Test()]
+        public void CtorTest_OSDNameEmptyThrows()
+        {
+            Assert.Throws<ArgumentException>(() => _ = new CecDevice(DeviceType.PlaybackDevice, "", 999,
+                PhysicalAddress.Parse("2.1.0.0"), LogicalAddress.Tuner3));
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_SetMenuLanguage()
+        {
+            var device = CreatePlaybackDevice();
+            Assert.AreEqual(CultureInfo.CurrentUICulture.ThreeLetterISOLanguageName, device.Language);
+
+            var message = CecMessageBuilder.SetMenuLanguage(LogicalAddress.TV, "heb");
+            var response = device.ProcessCecMessage(message);
+
+            Assert.AreEqual(CecMessage.None, response);
+            Assert.AreEqual("heb", device.Language);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_SetOSDString()
+        {
+            var device = CreateTVDevice();
+            var message = CecMessageBuilder.SetOSDString(LogicalAddress.Tuner1, DisplayControl.DisplayForDefaultTime,"Hello");
+            var response = device.ProcessCecMessage(message);
+            Assert.AreEqual(CecMessage.None, response);
+        }
+
+
+        [Test()]
+        public void ProcessCecMessageTest_GetMenuLanguage_PlaybackDevice()
+        {
+            var device = CreatePlaybackDevice();
+            var message = CecMessageBuilder.GetMenuLanguage(LogicalAddress.TV, device.LogicalAddress);
+            var response = device.ProcessCecMessage(message);
+
+            var expected = CecMessageBuilder.FeatureAbort(device.LogicalAddress, message.Source,
+                Command.GetMenuLanguage, AbortReason.UnrecognizedOpcode);
+
+            Assert.AreEqual(expected, response);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_GetMenuLanguage_Broadcast()
+        {
+            var device = CreatePlaybackDevice();
+            var message = CecMessageBuilder.GetMenuLanguage(LogicalAddress.TV, LogicalAddress.Unregistered);
+            var response = device.ProcessCecMessage(message);
+
+            Assert.AreEqual(Command.None, response.Command);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_GetMenuLanguage_TV()
+        {
+            var device = CreateTVDevice();
+            var message = CecMessageBuilder.GetMenuLanguage(LogicalAddress.Tuner3, device.LogicalAddress);
+            var response = device.ProcessCecMessage(message);
+            
+            var expected = CecMessageBuilder.SetMenuLanguage(device.LogicalAddress, device.Language);
+            Assert.AreEqual(expected, response);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_GetMenuLanguage_FreeUseTV()
+        {
+            var device = CreateTVDevice();
+            var message = CecMessageBuilder.GetMenuLanguage(LogicalAddress.Tuner3, device.LogicalAddress);
+            var response = device.ProcessCecMessage(message);
+
+            var expected = CecMessageBuilder.SetMenuLanguage(device.LogicalAddress, device.Language);
+            Assert.AreEqual(expected, response);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_GivePhysicalAddress()
+        {
+            var device = CreatePlaybackDevice();
+            var message = CecMessageBuilder.GivePhysicalAddress(LogicalAddress.TV, device.LogicalAddress);
+            var response = device.ProcessCecMessage(message);
+
+            var expected = CecMessageBuilder.ReportPhysicalAddress(device.LogicalAddress, device.DeviceType, device.PhysicalAddress);
+            Assert.AreEqual(expected, response);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_GiveOSDName()
+        {
+            var device = CreatePlaybackDevice();
+            var message = CecMessageBuilder.GiveOsdName(LogicalAddress.TV, device.LogicalAddress);
+            var response = device.ProcessCecMessage(message);
+
+            var expected = CecMessageBuilder.SetOsdName(device.LogicalAddress, message.Source, device.OsdName);
+            Assert.AreEqual(expected, response);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_SetOSDName()
+        {
+            var device = CreateTVDevice();
+
+            var message = CecMessageBuilder.SetOsdName(LogicalAddress.Tuner3, device.LogicalAddress, "Hello");
+            var response = device.ProcessCecMessage(message);
+            Assert.AreEqual(CecMessage.None, response);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_RequestActiveSource()
+        {
+            var device = CreatePlaybackDevice();
+            var message = CecMessageBuilder.SetOsdName(LogicalAddress.Tuner3, device.LogicalAddress, "Hello");
+            var response = device.ProcessCecMessage(message);
+            Assert.AreEqual(CecMessage.None, response);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_SetStreamPath()
+        {
+            var device = CreatePlaybackDevice();
+            var message = CecMessageBuilder.SetStreamPath(device.PhysicalAddress);
+            var response = device.ProcessCecMessage(message);
+            var expected = CecMessageBuilder.ActiveSource(device.LogicalAddress, device.PhysicalAddress);
+            Assert.AreEqual(expected, response);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_RoutingChange()
+        {
+            var device = CreatePlaybackDevice();
+            var message = CecMessageBuilder.RoutingChange(LogicalAddress.Reserved1, PhysicalAddress.Parse("3.0.0.0"),
+                device.PhysicalAddress);
+            var response = device.ProcessCecMessage(message);
+            Assert.AreEqual(CecMessage.None, response);
+            Assert.IsTrue(device.IsActiveSource);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_RoutingInformationThrows()
+        {
+            var device = CreatePlaybackDevice();
+            var message = CecMessageBuilder.RoutingInformation(LogicalAddress.Reserved1, PhysicalAddress.Parse("3.0.0.0"));
+            Assert.Throws<NotSupportedException>(() => _ = device.ProcessCecMessage(message));
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_GiveDevicePowerStatus()
+        {
+            var device = CreatePlaybackDevice();
+            var message = CecMessageBuilder.GiveDevicePowerStatus(LogicalAddress.TV, device.LogicalAddress);
+            var response = device.ProcessCecMessage(message);
+            var expected = CecMessageBuilder.ReportPowerStatus(device.LogicalAddress, message.Source, device.PowerStatus);
+            Assert.AreEqual(expected, response);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_DeviceVendorId()
+        {
+            var device = CreatePlaybackDevice();
+            var message = CecMessageBuilder.DeviceVendorId(LogicalAddress.TV, 999);
+            var response = device.ProcessCecMessage(message);
+            Assert.AreEqual(CecMessage.None, response);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_GiveDeviceVendorId()
+        {
+            var device = CreatePlaybackDevice();
+            var message = CecMessageBuilder.GiveDeviceVendorId(LogicalAddress.TV, device.LogicalAddress);
+            var response = device.ProcessCecMessage(message);
+            var expected = CecMessageBuilder.DeviceVendorId(device.LogicalAddress, device.VendorId);
+            Assert.AreEqual(expected, response);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_VendorCommand()
+        {
+            var device = CreatePlaybackDevice();
+            var message = CecMessageBuilder.VendorCommand(LogicalAddress.TV, device.LogicalAddress,"Hello");
+            var response = device.ProcessCecMessage(message);
+            Assert.AreEqual(CecMessage.None, response);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_VendorCommandWithId()
+        {
+            var device = CreatePlaybackDevice();
+            var message = CecMessageBuilder.VendorCommandWithId(LogicalAddress.TV, device.LogicalAddress, 999,"Hello");
+            var response = device.ProcessCecMessage(message);
+            Assert.AreEqual(CecMessage.None, response);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_VendorRemoteButtonDown()
+        {
+            var device = CreatePlaybackDevice();
+            var message = CecMessageBuilder.VendorRemoteButtonDown(LogicalAddress.TV, device.LogicalAddress, "Hello");
+            var response = device.ProcessCecMessage(message);
+            Assert.AreEqual(CecMessage.None, response);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_VendorRemoteButtonUp()
+        {
+            var device = CreatePlaybackDevice();
+            var message = CecMessageBuilder.VendorRemoteButtonUp(LogicalAddress.TV, device.LogicalAddress);
+            var response = device.ProcessCecMessage(message);
+            Assert.AreEqual(CecMessage.None, response);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_DirectlyAddressedStandby()
+        {
+            var device = CreatePlaybackDevice();
+            var message = CecMessageBuilder.Standby(LogicalAddress.TV, device.LogicalAddress);
+            var response = device.ProcessCecMessage(message);
+            Assert.AreEqual(CecMessage.None, response);
+            Assert.AreEqual(PowerStatus.Standby, device.PowerStatus);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_BroadcastStandby()
+        {
+            var device = CreatePlaybackDevice();
+            var message = CecMessageBuilder.Standby(LogicalAddress.TV, LogicalAddress.Unregistered);
+            var response = device.ProcessCecMessage(message);
+            Assert.AreEqual(CecMessage.None, response);
+            Assert.AreEqual(PowerStatus.Standby, device.PowerStatus);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_UserControlPressed()
+        {
+            var device = CreatePlaybackDevice();
+            var message = CecMessageBuilder.UserControlPressed(LogicalAddress.TV, device.LogicalAddress, UiCommand.RootMenu);
+            var response = device.ProcessCecMessage(message);
+            Assert.AreEqual(CecMessage.None, response);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_UserControlReleased()
+        {
+            var device = CreatePlaybackDevice();
+            var message = CecMessageBuilder.UserControlReleased(LogicalAddress.TV, device.LogicalAddress);
+            var response = device.ProcessCecMessage(message);
+            Assert.AreEqual(CecMessage.None, response);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_MenuRequestActivate()
+        {
+            var device = CreatePlaybackDevice();
+            var message = CecMessageBuilder.MenuRequest(device.LogicalAddress, MenuRequestType.Activate);
+            var response = device.ProcessCecMessage(message);
+            var expected = CecMessageBuilder.MenuStatus(device.LogicalAddress, MenuState.Activated);
+            Assert.AreEqual(expected, response);
+            Assert.AreEqual(MenuState.Activated, device.MenuState);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_MenuRequestDeactivate()
+        {
+            var device = CreatePlaybackDevice();
+            var message = CecMessageBuilder.MenuRequest(device.LogicalAddress, MenuRequestType.Deactivate);
+            var response = device.ProcessCecMessage(message);
+            var expected = CecMessageBuilder.MenuStatus(device.LogicalAddress, MenuState.Deactivated);
+            Assert.AreEqual(expected, response);
+            Assert.AreEqual(MenuState.Deactivated, device.MenuState);
+        }
+
+        [Test()]
+        public void ProcessCecMessageTest_MenuRequestQuery()
+        {
+            var device = CreatePlaybackDevice();
+            device.MenuState = MenuState.Activated;
+            var message = CecMessageBuilder.MenuRequest(device.LogicalAddress, MenuRequestType.Query);
+            var response = device.ProcessCecMessage(message);
+            var expected = CecMessageBuilder.MenuStatus(device.LogicalAddress, MenuState.Activated);
+            Assert.AreEqual(expected, response);
+        }
+
+        private static CecDevice CreatePlaybackDevice()
+        {
+            return new CecDevice(DeviceType.PlaybackDevice, "PlaybackDevice", 999, PhysicalAddress.Parse("2.0.0.0"), LogicalAddress.PlaybackDevice1);
+        }
+
+        private static CecDevice CreateTVDevice()
+        {
+            return new CecDevice(DeviceType.TV, "TVDevice", 999, PhysicalAddress.Parse("1.0.0.0"), LogicalAddress.TV);
+        }
+        
     }
 }
--- AleRoe.CecSharp.Tests/CecMessageBuilderTests.cs
+++ AleRoe.CecSharp.Tests/CecMessageBuilderTests.cs
@@ -9,28 +9,82 @@ namespace AleRoe.CecSharp.Tests
     [TestFixture()]
     public class CecMessageBuilderTests
     {
-        [Test, TestCaseSource(typeof(BuilderCases))]
+        [Test]
+        [TestCaseSource(nameof(BuilderTestData))]
         public void BuilderTest(CecMessage message, string expected)
         {
             Assert.AreEqual(expected, message.ToCec());
         }
-    }
-    class BuilderCases : IEnumerable<TestCaseData>
-    {
-        public IEnumerator<TestCaseData> GetEnumerator()
+
+        //[Test]
+        //[TestCaseSource(typeof(TestData), nameof(TestData.DevicePowerStatusFeatures), Category = "DevicePowerStatusFeatures")]
+        //[TestCaseSource(typeof(TestData), nameof(TestData.DeviceOSDNameTransferFeatures), Category = "DeviceOSDNameTransferFeatures")]
+        //public string BuilderTests(CecMessage message)
+        //{
+        //    return message.ToCec();
+        //}
+
+
+        private static IEnumerable<TestCaseData> BuilderTestData
         {
-            yield return new TestCaseData(CecMessageBuilder.ReportPowerStatus(LogicalAddress.Unregistered, LogicalAddress.Unregistered, PowerStatus.On), "FF:90:00") {TestName = nameof(CecMessageBuilder.ReportPowerStatus)};
-            yield return new TestCaseData(CecMessageBuilder.ActiveSource(LogicalAddress.Unregistered, PhysicalAddress.Parse("2.0.0.0")), "FF:82:20:00") { TestName = nameof(CecMessageBuilder.ActiveSource) };
-            yield return new TestCaseData(CecMessageBuilder.CecVersion(LogicalAddress.Unregistered, LogicalAddress.Unregistered, CecVersion.Version13A), "FF:9E:04") { TestName = nameof(CecMessageBuilder.CecVersion) };
-            yield return new TestCaseData(CecMessageBuilder.SetSystemAudioMode(LogicalAddress.Unregistered, LogicalAddress.Unregistered, SystemAudioStatus.Off), "FF:72:00") { TestName = nameof(CecMessageBuilder.SetSystemAudioMode) };
-            yield return new TestCaseData(CecMessageBuilder.SetSystemAudioMode(LogicalAddress.Unregistered, LogicalAddress.Unregistered, SystemAudioStatus.On), "FF:72:01") { TestName = nameof(CecMessageBuilder.SetSystemAudioMode) };
-            yield return new TestCaseData(CecMessageBuilder.ReportAudioStatus(LogicalAddress.Unregistered, LogicalAddress.Unregistered, AudioMuteStatus.AudioMuteOff, 127), "FF:7A:7F") { TestName = nameof(CecMessageBuilder.ReportAudioStatus) };
-            yield return new TestCaseData(CecMessageBuilder.ReportAudioStatus(LogicalAddress.Unregistered, LogicalAddress.Unregistered, AudioMuteStatus.AudioMuteOn, 3), "FF:7A:83") { TestName = nameof(CecMessageBuilder.ReportAudioStatus) };
+            get
+            {
+                yield return new TestCaseData(CecMessageBuilder.ReportPowerStatus(LogicalAddress.Unregistered, LogicalAddress.Unregistered, PowerStatus.On), "FF:90:00") { TestName = nameof(CecMessageBuilder.ReportPowerStatus) };
+                yield return new TestCaseData(CecMessageBuilder.CecVersion(LogicalAddress.Unregistered, LogicalAddress.Unregistered, CecVersion.Version13A), "FF:9E:04") { TestName = nameof(CecMessageBuilder.CecVersion) };
+                yield return new TestCaseData(CecMessageBuilder.SetSystemAudioMode(LogicalAddress.Unregistered, LogicalAddress.Unregistered, SystemAudioStatus.Off), "FF:72:00") { TestName = nameof(CecMessageBuilder.SetSystemAudioMode) };
+                yield return new TestCaseData(CecMessageBuilder.SetSystemAudioMode(LogicalAddress.Unregistered, LogicalAddress.Unregistered, SystemAudioStatus.On), "FF:72:01") { TestName = nameof(CecMessageBuilder.SetSystemAudioMode) };
+                yield return new TestCaseData(CecMessageBuilder.ReportAudioStatus(LogicalAddress.Unregistered, LogicalAddress.Unregistered, AudioMuteStatus.AudioMuteOff, 127), "FF:7A:7F") { TestName = nameof(CecMessageBuilder.ReportAudioStatus) };
+                yield return new TestCaseData(CecMessageBuilder.ReportAudioStatus(LogicalAddress.Unregistered, LogicalAddress.Unregistered, AudioMuteStatus.AudioMuteOn, 3), "FF:7A:83") { TestName = nameof(CecMessageBuilder.ReportAudioStatus) };
+                yield return new TestCaseData(CecMessageBuilder.SetMenuLanguage(LogicalAddress.TV, "heb"), "0F:32:68:65:62") { TestName = nameof(CecMessageBuilder.SetMenuLanguage) };
+                yield return new TestCaseData(CecMessageBuilder.GetMenuLanguage(LogicalAddress.TV, LogicalAddress.Tuner3), "07:91") { TestName = nameof(CecMessageBuilder.GetMenuLanguage) };
+                yield return new TestCaseData(CecMessageBuilder.FeatureAbort(LogicalAddress.Tuner3, LogicalAddress.TV, Command.GetMenuLanguage, AbortReason.UnrecognizedOpcode), "70:00:91:00") { TestName = nameof(CecMessageBuilder.FeatureAbort) };
+                yield return new TestCaseData(CecMessageBuilder.ReportPhysicalAddress(LogicalAddress.Tuner3, DeviceType.PlaybackDevice, PhysicalAddress.Parse("2.0.0.0")), "7F:84:20:00:04") { TestName = nameof(CecMessageBuilder.ReportPhysicalAddress) };
+                yield return new TestCaseData(CecMessageBuilder.SetOSDString(LogicalAddress.Tuner3, DisplayControl.DisplayForDefaultTime, "Hello"), "70:64:00:48:65:6C:6C:6F") { TestName = nameof(CecMessageBuilder.SetOSDString) };
+                yield return new TestCaseData(CecMessageBuilder.SetOSDString(LogicalAddress.Tuner3, DisplayControl.DisplayUntilCleared, "Hello"), "70:64:40:48:65:6C:6C:6F") { TestName = nameof(CecMessageBuilder.SetOSDString) };
+                yield return new TestCaseData(CecMessageBuilder.SetOSDString(LogicalAddress.Tuner3, DisplayControl.ClearPreviousMessage, "Hello"), "70:64:80:48:65:6C:6C:6F") { TestName = nameof(CecMessageBuilder.SetOSDString) };
+                yield return new TestCaseData(CecMessageBuilder.SetOSDString(LogicalAddress.Tuner3, DisplayControl.ReservedForFutureUse, "Hello"), "70:64:C0:48:65:6C:6C:6F") { TestName = nameof(CecMessageBuilder.SetOSDString) };
+                yield return new TestCaseData(CecMessageBuilder.GiveOsdName(LogicalAddress.TV, LogicalAddress.Tuner3), "07:46") { TestName = nameof(CecMessageBuilder.GiveOsdName) };
+                yield return new TestCaseData(CecMessageBuilder.SetOsdName(LogicalAddress.Tuner3, LogicalAddress.TV, "MyName"), "70:47:4D:79:4E:61:6D:65") { TestName = nameof(CecMessageBuilder.SetOsdName) };
+                yield return new TestCaseData(CecMessageBuilder.ActiveSource(LogicalAddress.Unregistered, PhysicalAddress.Parse("2.0.0.0")), "FF:82:20:00") { TestName = nameof(CecMessageBuilder.ActiveSource) };
+                yield return new TestCaseData(CecMessageBuilder.InactiveSource(LogicalAddress.Tuner3, PhysicalAddress.Parse("2.0.0.0")), "70:9D:20:00") { TestName = nameof(CecMessageBuilder.InactiveSource) };
+                yield return new TestCaseData(CecMessageBuilder.RequestActiveSource(LogicalAddress.Tuner3), "7F:85") { TestName = nameof(CecMessageBuilder.RequestActiveSource) };
+                yield return new TestCaseData(CecMessageBuilder.SetStreamPath(PhysicalAddress.Parse("2.0.0.0")), "0F:86:20:00") { TestName = nameof(CecMessageBuilder.SetStreamPath) };
+                yield return new TestCaseData(CecMessageBuilder.RoutingChange(LogicalAddress.Reserved1, PhysicalAddress.Parse("2.0.0.0"), PhysicalAddress.Parse("2.1.0.0")), "CF:80:20:00:21:00") { TestName = nameof(CecMessageBuilder.RoutingChange) };
+                yield return new TestCaseData(CecMessageBuilder.RoutingInformation(LogicalAddress.Reserved1, PhysicalAddress.Parse("2.0.0.0")), "CF:81:20:00") { TestName = nameof(CecMessageBuilder.RoutingInformation) };
+                yield return new TestCaseData(CecMessageBuilder.GiveDevicePowerStatus(LogicalAddress.TV, LogicalAddress.PlaybackDevice1), "04:8F").SetCategory(Features.DevicePowerStatus.ToString()).SetName(nameof(CecMessageBuilder.GiveDevicePowerStatus));
+                yield return new TestCaseData(CecMessageBuilder.ReportPowerStatus(LogicalAddress.PlaybackDevice1, LogicalAddress.TV, PowerStatus.On), "40:90:00").SetCategory(Features.DevicePowerStatus.ToString()).SetName(nameof(CecMessageBuilder.ReportPowerStatus));
+                yield return new TestCaseData(CecMessageBuilder.DeviceVendorId(LogicalAddress.PlaybackDevice1, 999), "4F:87:00:03:E7").SetCategory(Features.VendorSpecificCommand.ToString()).SetName(nameof(CecMessageBuilder.DeviceVendorId));
+                yield return new TestCaseData(CecMessageBuilder.GiveDeviceVendorId(LogicalAddress.TV, LogicalAddress.PlaybackDevice3), "0B:8C").SetCategory(Features.VendorSpecificCommand.ToString()).SetName(nameof(CecMessageBuilder.GiveDeviceVendorId));
+                yield return new TestCaseData(CecMessageBuilder.VendorCommand(LogicalAddress.TV, LogicalAddress.PlaybackDevice3, "Hello"), "0B:89:48:65:6C:6C:6F").SetCategory(Features.VendorSpecificCommand.ToString()).SetName(nameof(CecMessageBuilder.VendorCommand));
+                yield return new TestCaseData(CecMessageBuilder.VendorCommandWithId(LogicalAddress.TV, LogicalAddress.PlaybackDevice3, 999, "Hello"), "0B:A0:00:03:E7:48:65:6C:6C:6F").SetCategory(Features.VendorSpecificCommand.ToString()).SetName(nameof(CecMessageBuilder.VendorCommandWithId));
+                yield return new TestCaseData(CecMessageBuilder.VendorRemoteButtonDown(LogicalAddress.TV, LogicalAddress.PlaybackDevice3, "Hello"), "0B:8A:48:65:6C:6C:6F").SetCategory(Features.VendorSpecificCommand.ToString()).SetName(nameof(CecMessageBuilder.VendorRemoteButtonDown));
+                yield return new TestCaseData(CecMessageBuilder.Standby(LogicalAddress.TV, LogicalAddress.PlaybackDevice3), "0B:36").SetCategory(Features.SystemStandby.ToString()).SetName(nameof(CecMessageBuilder.Standby));
+                yield return new TestCaseData(CecMessageBuilder.UserControlPressed(LogicalAddress.TV, LogicalAddress.PlaybackDevice3, UiCommand.RootMenu), "0B:44:09").SetCategory(Features.DeviceMenuControl.ToString()).SetName(nameof(CecMessageBuilder.UserControlPressed));
+                yield return new TestCaseData(CecMessageBuilder.UserControlReleased(LogicalAddress.TV, LogicalAddress.PlaybackDevice3), "0B:45").SetCategory(Features.DeviceMenuControl.ToString()).SetName(nameof(CecMessageBuilder.UserControlReleased));
+                yield return new TestCaseData(CecMessageBuilder.MenuRequest(LogicalAddress.PlaybackDevice3, MenuRequestType.Query), "0B:8D:02").SetCategory(Features.DeviceMenuControl.ToString()).SetName(nameof(CecMessageBuilder.MenuRequest));
+                yield return new TestCaseData(CecMessageBuilder.MenuStatus(LogicalAddress.PlaybackDevice3, MenuState.Deactivated), "B0:8E:01").SetCategory(Features.DeviceMenuControl.ToString()).SetName(nameof(CecMessageBuilder.MenuStatus));
+            }
+        }
+    }
+
+    class TestData
+    {
+        public static IEnumerable<TestCaseData> DevicePowerStatusFeatures
+        {
+            get
+            {
+                yield return new TestCaseData(CecMessageBuilder.GiveDevicePowerStatus(LogicalAddress.TV, LogicalAddress.PlaybackDevice1)).Returns("04:8F").SetName(nameof(CecMessageBuilder.GiveDevicePowerStatus));
+                yield return new TestCaseData(CecMessageBuilder.ReportPowerStatus(LogicalAddress.PlaybackDevice1, LogicalAddress.TV, PowerStatus.On)).Returns("40:90:00").SetName(nameof(CecMessageBuilder.ReportPowerStatus));
+            }
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
+        public static IEnumerable<TestCaseData> DeviceOSDNameTransferFeatures
         {
-            return GetEnumerator();
+            get
+            {
+                yield return new TestCaseData(CecMessageBuilder.GiveOsdName(LogicalAddress.TV, LogicalAddress.Tuner3)).Returns("07:46");
+                yield return new TestCaseData(CecMessageBuilder.SetOsdName(LogicalAddress.Tuner3, LogicalAddress.TV, "MyName")).Returns("70:47:4D:79:4E:61:6D:65").SetName(nameof(CecMessageBuilder.SetOsdName));
+            }
         }
     }
 }
