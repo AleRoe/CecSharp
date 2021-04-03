c7d6019be64689a47b3491422d219939c94f7023
Alexander Röthinger <alexander1967muc@hotmail.com>
2021-04-04 00:04:17 +0200

    Added DisplayControl

---
+++ AleRoe.CecSharp/Model/DisplayControl.cs
@@ -0,0 +1,10 @@
+﻿namespace AleRoe.CecSharp.Model
+{
+    public enum DisplayControl : byte
+    {
+        DisplayForDefaultTime = 0x00,
+        DisplayUntilCleared = 0x40,
+        ClearPreviousMessage = 0x80,
+        ReservedForFutureUse = 0xC0
+    }
+}
